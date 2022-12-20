d54560718e091c1a220927aaaca316ec037bbda5
Nastassia Patin <nvpatin@gmail.com>
2022-12-19 19:06:45 -0800

    Edited amplicon preprocessing script to work with very large single matrix output of ranRelPct

--- ranRelPct_example.R
+++ ranRelPct_example.R
@@ -1,28 +1,35 @@
+#! /usr/local/bin/Rscript
+library(here)
+
 rm(list = ls())
-source("ranRelPct.R")
+source("/Users/nastassia.patin/GitHub/NOAA-NCAR-Hackathon/ranRelPct.R")
 
 # Example occurrence data -------------------------------------------------
 
-num.samples <- 5
-num.otus <- 10
+#num.samples <- 5
 
-num.reads <- matrix(
-  sample(0:30, num.samples * num.otus, replace = TRUE),
-  ncol = num.samples
-)
+#num.otus <- 10
+
+#num.reads <- matrix(
+#  sample(0:30, num.samples * num.otus, replace = TRUE),
+#  ncol = num.samples
+#)
 
 
 # Test it -----------------------------------------------------------------
 
 # This is the mode (expected value) of the distribution
-rel.pct <- t(t(num.reads) / colSums(num.reads))
+#rel.pct <- t(t(num.reads) / colSums(num.reads))
 
 # One random draw
-ran.pct <- ranRelPct(num.reads)
+#ran.pct <- ranRelPct(num.reads)
 
 
 # Lasker 2018 data --------------------------------------------------------
-
-lasker2018 <- read.delim("Data/Lasker2018_table_counts.tsv", row.names = 1)
+path <- here::here("Data", "Lasker2018_table_counts.tsv")
+lasker2018 <- read.delim(path, row.names = 1)
 
 ran.lasker <- ranRelPct(lasker2018)
+
+options(max.print=2510000)
+print(ran.lasker)
