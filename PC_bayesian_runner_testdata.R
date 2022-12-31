87f6227d3b1f542801c406d2f9129d1c766d4f7e
Nastassia Patin <nvpatin@gmail.com>
2022-12-31 15:48:20 -0800

    Modified multitrophic_Bayesian notebook to perform PCA in-line and pass PCs to R script

--- PC_bayesian_runner_testdata.R
+++ PC_bayesian_runner_testdata.R
@@ -10,34 +10,27 @@ source(here::here("jagsPClm.R"))
 args <- commandArgs(trailingOnly=TRUE)
 
 # Load data ---------------------------------------------------------------
-fl16s.prob <- read.delim(here::here("Data", args[3]), row.names = 1)
-fl18s.prob <- read.delim(here::here("Data", args[4]), row.names = 1)
-
-# Extract 16s PCs ---------------------------------------------------------
-fl16s.lo <- log(fl16s.prob / (1 - fl16s.prob))
-# using 'summary()' function to get variances of each component
-fl16s.pca <- summary(prcomp(fl16s.lo))
-fl16s.imp <- fl16s.pca$importance
-
-# Extract 18s PCs ---------------------------------------------------------
-# !!!! Make sure to sort the the 18s data so the samples are in the same order
-# as the 16s data
-fl18s.lo <- log(fl18s.prob / (1 - fl18s.prob))
-fl18s.pca <- summary(prcomp(fl18s.lo))
+pc.16s <- as.matrix(read.delim(here::here("Data", 
+                                    "Flyer2018_16S_PCs.tsv"), 
+                         row.names = 1))
+pc.18s <- as.matrix(read.delim(here::here("Data", 
+                                    "Flyer2018_18S_PCs.tsv"), 
+                         row.names = 1))
 
 # number of PCs to predict in 18S data
-num.18s.pc <- 2
+num.18s.pc <- strtoi(args[3])
 # number of predictor PCs to use from 16S data
 # = as many as account for expected variance of predictor
-num.preds <- which.min(fl16s.imp["Proportion of Variance", ] >= (1 / ncol(fl16s.imp)))
-num.preds <- max(1, num.preds - 1)
+num.preds <- strtoi(args[4])
+# number of observances (samples)
+num.ind <- strtoi(args[5])
 
 invisible(capture.output(post.smry <- jagsPClm(
-  num.ind = nrow(fl18s.pca$x),
+  num.ind = num.ind,
   num.18s.pc = num.18s.pc,
   num.preds = num.preds,
-  pc.18s = fl18s.pca$x[, 1:num.18s.pc],
-  pc.16s = fl16s.pca$x[, 1:num.preds],
+  pc.18s = pc.18s,
+  pc.16s = pc.16s,
   chains = 10,
   adapt = 500,
   burnin = 10000,
@@ -48,7 +41,7 @@ invisible(capture.output(post.smry <- jagsPClm(
 df <- as.data.frame(post.smry)
 df$rownames <- rownames(df)
 df <- df %>% relocate(rownames, .before = Lower95)
-df[46,] <- colnames(df)
+df[18,] <- colnames(df) # This needs to change based on number of 16S predictors; 18 is for 6 predictors
 df <- df[order(df$rownames=='rownames', decreasing=TRUE), ]
 
 smry.forpy <- as.vector(t(df))
