90efed75ceb9bdcce043e492b7bb43cb74be4fc0
Eric Archer <eric.archer@noaa.gov>
2023-02-20 21:43:28 -0800

    added harmonizeColumnSign() to make all columns of a list of matrices have same sign as first matrix - for comparing loadings from multiple PCAs

--- AccessoryScripts/erics_sandbox/Python/MarineDNA.py
+++ AccessoryScripts/erics_sandbox/Python/MarineDNA.py
@@ -7,3 +7,10 @@ def ranRelPct(mat):
         return ran.betavariate(a, total - a)
     p = [[drawOneSample(count, sum(row)) for count in row] for row in mat]
     return(np.array(p))
+
+def harmonizeColumnSigns(mat_list):
+    for i in range(1, len(mat_list)):
+        for col in range(mat_list[i].shape[1]):
+            if np.sign(mat_list[i][0, col]) != np.sign(mat_list[0][0, col]):
+                mat_list[i][:, col] *= -1
+    return(mat_list)
