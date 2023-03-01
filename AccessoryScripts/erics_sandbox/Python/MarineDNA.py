9f6237b5afc296a1a6bc2d6c4229fdf1f61cf233
root <n.v.patin@gmail.com>
2023-03-01 18:45:13 +0000

    Hackathon Day 3 updates

--- AccessoryScripts/erics_sandbox/Python/MarineDNA.py
+++ AccessoryScripts/erics_sandbox/Python/MarineDNA.py
@@ -2,19 +2,17 @@
 # fit to observed occurrence counts
 #   df: data frame where rows = ASVs and columns = samples
 def ranRelPct(df, asLogOdds = True):
-    import numpy as np
-    
     result = df.copy()
     for i in range(df.shape[1]):
         col = df.iloc[:,i]
         a = col + 1
         b = col.sum() - col + 1
-        beta_dist = np.random.beta(a,b)
+        beta_dist = cupy.random.beta(a,b)
         beta_dist /= beta_dist.sum()
         result.iloc[:,i] = beta_dist
     # convert to log-odds if requested
     if asLogOdds:
-        result = np.log(result / (1 - result))
+        result = cupy.log(result / (1 - result))
     return result.transpose()
 
 
@@ -25,10 +23,6 @@ def ranRelPct(df, asLogOdds = True):
 #   scores: array of PCA scores
 #   loadings: array of PCA loadings
 def doPCA(df, num_pcs = None):
-    import pandas as pd
-    from sklearn.decomposition import PCA
-    import numpy as np
-    
     max_pcs = min(df.shape[0] - 1, df.shape[1] - 1)
     if num_pcs is None:
         num_pcs = max_pcs
@@ -38,7 +32,7 @@ def doPCA(df, num_pcs = None):
     pca_fit = pca.fit(df)
     pca_results = {
         "scores": pca_fit.transform(df),
-        "loadings": np.transpose(pca_fit.components_)
+        "loadings": cp.transpose(pca_fit.components_)
     }
     return pca_results
 
@@ -46,32 +40,26 @@ def doPCA(df, num_pcs = None):
 # If the sign of the first element in a column in matrices after the first is different than the first,
 # multiply all values in that column by -1
 def harmonizeColumnSigns(mat_list):
-    import numpy as np
-    
     for i in range(1, len(mat_list)):
         for col in range(mat_list[i].shape[1]):
-            if np.sign(mat_list[i][0, col]) != np.sign(mat_list[0][0, col]):
+            if cp.sign(mat_list[i][0, col]) != cp.sign(mat_list[0][0, col]):
                 mat_list[i][:, col] *= -1
     return mat_list
 
 
 # Sorts PCA loadings from a list 
 def sortLoadings(loading_list, pc, asvs, asRanks = False):
-    import numpy as np
-    from scipy.stats import rankdata
-    import pandas as pd
-    
     # Harmonize signs across replicates
     harm_loadings = harmonizeColumnSigns(loading_list)
     # Create 3 dimensional array and select component 'pc'
-    loadings = np.stack(harm_loadings, axis = 2)[:, pc, :]
+    loadings = cp.stack(harm_loadings, axis = 2)[:, pc, :]
     # Convert to ranks if 'asRanks == True'
     if asRanks:
-        loadings = np.array([rankdata(loadings[:, i]) for i in range(loadings.shape[1])]).transpose()
+        loadings = cp.array([rankdata(loadings[:, i]) for i in range(loadings.shape[1])]).transpose()
     # Get sorted order based on median for each ASV 
-    row_sort = np.apply_along_axis(np.median, 1, loadings).ravel().argsort()[::-1]
+    row_sort = cp.apply_along_axis(np.median, 1, loadings).ravel().argsort()[::-1]
     # Sort based on median, add ASV names (also sorted) and return data frame
-    df = pd.DataFrame(loadings[row_sort, :])
+    df = cudf.DataFrame(loadings[row_sort, :])
     df.index = asvs[row_sort]
     return df
 
