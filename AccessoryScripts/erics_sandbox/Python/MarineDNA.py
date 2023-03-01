749e0a4dddff140c38fb7afd8296b27131c246bc
Eric Archer <eric.archer@noaa.gov>
2023-02-28 23:10:14 -0800

    changed samplePCA and sampleClust to doPCA and doClustering.
    removed call to ranRelPct from doPCA and doClustering
    
    streamlined cluster test Jupyter notebook

--- AccessoryScripts/erics_sandbox/Python/MarineDNA.py
+++ AccessoryScripts/erics_sandbox/Python/MarineDNA.py
@@ -1,68 +1,44 @@
-import numpy as np
-import random as ran
-import pandas as pd
-from sklearn.decomposition import PCA
-from scipy.stats import rankdata
-
-
-# Return data frame of a draw of relative percent of occurrence from a beta distribution
-# fit to observed occurrence counts
-#   df: data frame where rows = samples and columns = ASVs
-def ranRelPct(df, asLogOdds = True):
-    # function to return a random draw from a beta distribution for a row
-    def betaRow(row):
-        ran_row = np.random.beta(row + 1, row.sum() - row + 1)
-        ran_row = ran_row / ran_row.sum()
-        return ran_row
-    # apply function to every row to draw sample of relative percent occurrence
-    result = df.apply(betaRow, axis = 1, result_type = 'expand')
-    # assign row and column names
-    result.index = df.index.values
-    result.columns = df.columns.values
-    # convert to log-odds if requested
-    if asLogOdds:
-        result = np.log(result / (1 - result))
-    return result
-
-
 # Return data frame of a draw of relative percent of occurrence from a beta distribution
 # fit to observed occurrence counts
 #   df: data frame where rows = ASVs and columns = samples
-def ranRelPct_cupy(df, asLogOdds = True):
+def ranRelPct(df, asLogOdds = True):
+    import numpy as np
+    
+    result = df.copy()
     for i in range(df.shape[1]):
         col = df.iloc[:,i]
         a = col + 1
         b = col.sum() - col + 1
-        beta_dist = cupy.random.beta(a,b)
-        beta_dist /= col.sum()
-        df.iloc[:,i] = beta_dist
+        beta_dist = np.random.beta(a,b)
+        beta_dist /= beta_dist.sum()
+        result.iloc[:,i] = beta_dist
     # convert to log-odds if requested
     if asLogOdds:
-        result = np.log(df / (1 - df))
+        result = np.log(result / (1 - result))
     return result.transpose()
 
 
-# Draws sample of relative percent of occurrence and conducts PCA
+# Does PCA
 #   df: data frame where rows = samples and columns = ASVs
+#   num_pcs: number of components to return. if None, return maximum number
 # Returns a dictionary containing :
-#   df: data frame containing a random draw of the original count data frame as log-odds
 #   scores: array of PCA scores
 #   loadings: array of PCA loadings
-def samplePCA(df, num_pcs = None):
+def doPCA(df, num_pcs = None):
+    import pandas as pd
+    from sklearn.decomposition import PCA
+    import numpy as np
+    
     max_pcs = min(df.shape[0] - 1, df.shape[1] - 1)
     if num_pcs is None:
         num_pcs = max_pcs
     elif num_pcs > max_pcs:
         num_pcs = max_pcs
-    ran_df = ranRelPct(df, asLogOdds = True)
     pca = PCA(n_components = num_pcs)
-    pca_fit = pca.fit(ran_df)
-    scores = pca_fit.transform(ran_df)
-    loadings = np.transpose(pca_fit.components_)
+    pca_fit = pca.fit(df)
     pca_results = {
-        "df": ran_df,
-        "scores": scores,
-        "loadings": loadings
+        "scores": pd.DataFrame(pca_fit.transform(df)),
+        "loadings": pd.DataFrame(np.transpose(pca_fit.components_))
     }
     return pca_results
 
@@ -70,6 +46,7 @@ def samplePCA(df, num_pcs = None):
 # If the sign of the first element in a column in matrices after the first is different than the first,
 # multiply all values in that column by -1
 def harmonizeColumnSigns(mat_list):
+    import numpy as np
     for i in range(1, len(mat_list)):
         for col in range(mat_list[i].shape[1]):
             if np.sign(mat_list[i][0, col]) != np.sign(mat_list[0][0, col]):
@@ -79,6 +56,8 @@ def harmonizeColumnSigns(mat_list):
 
 # Sorts PCA loadings from a list 
 def sortLoadings(loading_list, pc, asvs, asRanks = False):
+    import numpy as np
+    from scipy.stats import rankdata
     # Harmonize signs across replicates
     harm_loadings = harmonizeColumnSigns(loading_list)
     # Create 3 dimensional array and select component 'pc'
@@ -93,10 +72,10 @@ def sortLoadings(loading_list, pc, asvs, asRanks = False):
     df.index = asvs[row_sort]
     return df
 
-def sampleClust(df, n_clusts, num_pcs = None):
+# Does hierarchical clustering on data frame where rows are samples and columns are ASVs
+# Returns array of cluster labels for rows
+def doClustering(df, num_clusts, num_pcs = None):
     from sklearn.cluster import AgglomerativeClustering
-    x = samplePCA(df, num_pcs = None)
-    agg_clust = AgglomerativeClustering(n_clusters = n_clusts, metric = "euclidean", linkage = "ward")
-    labels = agg_clust.fit_predict(x["df"])
-    x["cluster"] = labels.astype(str)
-    return x
+    agg_clust = AgglomerativeClustering(n_clusters = num_clusts, metric = "euclidean", linkage = "ward")
+    labels = agg_clust.fit_predict(df)
+    return labels.astype(str)
