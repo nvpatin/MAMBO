0d2a1d2d3c9a7f85b328e4a661a19d435fbdaadd
Nastassia Patin <nvpatin@gmail.com>
2023-04-13 16:00:55 -0700

    Updated clustering_RAPIDS script in preparation for testing on Google Cloud

--- MainScripts/.ipynb_checkpoints/MarineDNA_RAPIDS-checkpoint.py
+++ MainScripts/.ipynb_checkpoints/MarineDNA_RAPIDS-checkpoint.py
@@ -80,3 +80,146 @@ def doClustering(df, num_clusts, num_pcs = None):
                          linkage = "single", output_type="cudf")
     labels = agg_clust.fit_predict(df)
     return labels.astype(int)
+
+# A function to test if row pws[0] is the same cluster as row pws[1] in column 'col'
+def isSameCluster(pws, df, col):
+    return df.iloc[pws[0], col] == df.iloc[pws[1], col]
+
+print(isSameCluster((1, 3), cluster_samples, 0))
+print(isSameCluster((0, 3), cluster_samples, 0))
+
+# Function to create n_rep draws of df and assign n_clusters and returns percent of draws that had same relative 
+# cluster assignments
+def pctSame(df, n_clust, n_rep):
+    if n_clust >= df.shape[0]:
+        return 100
+    
+    def isSameCluster(pws, df, col):
+        return df.iloc[pws[0], col] == df.iloc[pws[1], col]
+    
+    def maxSame(row):
+        unique, counts = cp.unique(row, return_counts=True)
+        return max(counts)
+    
+    # cluster a random sample of logit(relative percentages)
+    cluster_samples = [md.doClustering(md.ranRelPct(df), n_clust) for i in range(n_rep)]
+    cluster_samples = cudf.DataFrame(cluster_samples).transpose()
+    # unique pairs of rows
+    tensor_a = torch.tensor(range(cluster_samples.shape[0]))
+    *pws_rows, = torch.combinations(tensor_a.cuda(), 2)
+    pws_same = cp.empty([len(pws_rows), n_rep])
+    for c in range(pws_same.shape[1]):
+        for r in range(pws_same.shape[0]):
+            pws_same[r, c] = cluster_samples.iloc[pws_rows[r][0], c] == cluster_samples.iloc[pws_rows[r][1], c]
+    
+    # get the maximum number replicates that have the same value (True or False) for each sample
+    num_same = cp.array([maxSame(pws_same[row, :]) for row in range(pws_same.shape[0])])
+    # convert to percentage with maximum of same value across all replicates
+    return cp.sum(num_same) * 100 / (pws_same.shape[0] * pws_same.shape[1])
+
+##################################################################################
+## Everything below this line still needs to be adapted for GPUs/RAPIDS
+##################################################################################
+
+# Returns a vector of logicals identifying values that are outside of the inter-quartile range
+def isOutlierIQR(x):
+    import numpy as np
+    
+    quarts = np.quantile(x, [0.25, 0.75])
+    iqr = np.diff(quarts)
+    lower = quarts[0] - 1.5 * iqr
+    upper = quarts[1] + 1.5 * iqr
+    thresh = [lower, upper]
+    return np.concatenate([xi <= thresh[0] or xi >= thresh[1] for xi in x])
+
+# Returns a vector of logicals identifying values that have Z-scores larger than the threshold
+def isOutlierZ(x, thresh = 3):
+    import scipy
+    
+    z = scipy.stats.zscore(x)
+    return abs(z) >= thresh
+
+
+def loadingSummary(loadings, pc, asv_labels, z_thresh = 3, pct_outlier_thresh = 0.95):
+    import numpy as np
+    import pandas
+    
+    metrics = {
+        'loadings': sortLoadings_std(loadings, 0, asv_labels),
+        'ranks': sortLoadings_std(loadings, 0, asv_labels, True)
+    }
+    metrics['outlier_IQR'] = metrics['loadings'].apply(isOutlierIQR)
+    metrics['outlier_Z'] = metrics['loadings'].apply(isOutlierZ, thresh = z_thresh)
+    
+    smry = pandas.DataFrame()
+    
+    smry['mean_loading'] = metrics['loadings'].apply(np.mean, axis = 1)
+    smry['median_loading'] = metrics['loadings'].apply(np.median, axis = 1)
+
+    smry['mean_rank'] = metrics['ranks'].apply(np.mean, axis = 1)
+    smry['median_rank'] = metrics['ranks'].apply(np.median, axis = 1)
+
+    smry['pct_outlier_IQR'] = metrics['outlier_IQR'].apply(np.mean, axis = 1)
+    smry['pct_outlier_Z'] = metrics['outlier_Z'].apply(np.mean, axis = 1)
+
+    return {
+        'metrics': metrics,
+        'smry': smry, 
+        'positive': smry[(smry['pct_outlier_Z'] > pct_outlier_thresh) & (smry['mean_loading'] > 0)],
+        'negative': smry[(smry['pct_outlier_Z'] > pct_outlier_thresh) & (smry['mean_loading'] < 0)].sort_values(by = 'mean_loading')
+    }
+
+
+def plotScoreDistribution(scores, x = 0, y = 1):
+    import pandas as pd
+    import numpy as np
+    import plotly.offline as pyo
+    import plotly.graph_objs as go
+    
+    score_list = md.harmonizeColumnSigns_std(scores)
+    score_arr = np.stack(score_list, axis = 2)
+
+    median_score = pd.DataFrame([[np.median(score_arr[row, col, :]) for col in range(score_arr.shape[1])] for row in range(score_arr.shape[0])])
+    min_score = pd.DataFrame([[np.min(score_arr[row, col, :]) for col in range(score_arr.shape[1])] for row in range(score_arr.shape[0])])
+    max_score = pd.DataFrame([[np.max(score_arr[row, col, :]) for col in range(score_arr.shape[1])] for row in range(score_arr.shape[0])])
+
+    medians = go.Scatter(
+        x = median_score.iloc[:, 0],
+        y = median_score.iloc[:, 1],
+        mode = 'markers'
+    )
+
+    horiz_lines = [
+        dict(
+            type = 'line',
+            x0 = min_score.iloc[i, 0],
+            y0 = median_score.iloc[i, 1],
+            x1 = max_score.iloc[i, 0],
+            y1 = median_score.iloc[i, 1],
+            line = dict(
+                color = 'grey',
+                width = 1
+            )
+        )
+        for i in range(median_score.shape[0])
+    ]
+
+    vert_lines = [
+        dict(
+            type = 'line',
+            x0 = median_score.iloc[i, 0],
+            y0 = min_score.iloc[i, 1],
+            x1 = median_score.iloc[i, 0],
+            y1 = max_score.iloc[i, 1],
+            line = dict(
+                color = 'grey',
+                width = 1
+            )
+        )
+        for i in range(median_score.shape[0])
+    ]
+
+    go.Figure(
+        medians, 
+        go.Layout(shapes = horiz_lines + vert_lines, autosize = False, width = 1000, height = 1000)
+    ).show()
