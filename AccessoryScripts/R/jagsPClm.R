5a1948637a7700d0f39b974a1a71528629408b3b
Eric Archer <eric.archer@noaa.gov>
2023-07-19 16:11:25 -0700

    bayesian model with model averaging weights

--- AccessoryScripts/R/jagsPClm.R
+++ AccessoryScripts/R/jagsPClm.R
@@ -8,9 +8,11 @@ jagsPClm <- function(num.ind, num.18s.pc, num.preds, pc.18s, pc.16s,
       # Prior for intercept
       intercept[p] ~ dnorm(0, 5e-4)
 
-      # Prior for ASV coefficients
+      # Prior for ASV coefficients and weights
       for(a in 1:num.preds) {
         b[a, p] ~ dnorm(0, 1e-6)
+        w[a, p] ~ dbern(0.5)
+        b.prime[a, p] <- b[a, p] * w[a, p]
       }
 
       # Prior for variance
@@ -20,12 +22,12 @@ jagsPClm <- function(num.ind, num.18s.pc, num.preds, pc.18s, pc.16s,
 
     for(i in 1:num.ind) {
       for(p in 1:num.18s.pc) {
-        mu[i, p] <- intercept[p] + inprod(b[, p], pc.16s[i, ])
+        mu[i, p] <- intercept[p] + inprod(b.prime[, p], pc.16s[i, ])
         pc.18s[i, p] ~ dnorm(mu[i, p], tau[p])
       }
     }
   }",
-    monitor = c("deviance", "intercept", "b", "v"),
+    monitor = c("deviance", "intercept", "b.prime", "w", "v"),
     data = list(
       num.ind = num.ind,
       num.18s.pc = num.18s.pc,
