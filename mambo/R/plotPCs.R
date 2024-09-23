e06581eeb7aaa29fc1dfcf6468b445b03f8cbe56
eric.archer <eric.archer@noaa.gov>
2024-09-23 13:26:05 -0700

    added 2-D density and % variance to plotPCs()

--- mambo/R/plotPCs.R
+++ mambo/R/plotPCs.R
@@ -5,7 +5,9 @@
 #' @param locus label name of response or predictor locus.
 #' @param pc.x number of x-axis principal component.
 #' @param pc.y number of y-axis principal component.
+#' @param type plot as ellipse of samples or 2-D density.
 #' @param ellipse.p probability density level of ellipse.
+#' @param num.bins number of bins for each axis if 2-D density is plotted.
 #' @param plot display plot?
 #'
 #' @return PCA biplot of scores with confidence ellipses for each sample.
@@ -14,31 +16,68 @@
 #'
 #' @export
 #'
-plotPCs <- function(results, locus, pc.x = 1, pc.y = 2, ellipse.p = 0.95, plot = TRUE) {
+plotPCs <- function(results, locus, pc.x = 1, pc.y = 2, 
+                    type = c('ellipse', 'density'), ellipse.p = 0.95, 
+                    num.bins = 50, plot = TRUE) {
+  if(missing(locus)) stop("'locus' must be specified.")
   scores <- extractPCA(results)$scores[[locus]]
   
-  gg <- purrr::imap(split(scores, scores$sample), function(df, i) {
-    x <- dplyr::filter(df, pc == pc.x)$score
-    y <- dplyr::filter(df, pc == pc.y)$score
-    car::dataEllipse(x, y, levels = ellipse.p, draw = FALSE) |> 
-      as.data.frame() |> 
-      dplyr::mutate(sample = i)
-  }) |> 
-    dplyr::bind_rows() |> 
-    ggplot2::ggplot() +
+  prop.var <- sapply(
+    results$reps, 
+    function(r) r$pca[[locus]]$importance['Proportion of Variance', c(pc.x, pc.y)]
+  ) |> 
+    t() |> 
+    apply(2, median)
+
+  type <- match.arg(type)
+  df <- if(type == 'ellipse') {
+    scores |> 
+      split(scores$sample) |> 
+      purrr::imap(function(df, i) {
+        x <- dplyr::filter(df, pc == pc.x)$score
+        y <- dplyr::filter(df, pc == pc.y)$score
+        car::dataEllipse(x, y, levels = ellipse.p, draw = FALSE) |> 
+          as.data.frame() |> 
+          dplyr::mutate(sample = i)
+      }) |> 
+      dplyr::bind_rows()
+  } else {
+    scores |> 
+      dplyr::mutate(axis = ifelse(pc == pc.x, 'x', 'y')) |> 
+      dplyr::filter(pc %in% c(pc.x, pc.y)) |> 
+      dplyr::select(-dplyr::all_of('pc')) |> 
+      tidyr::pivot_wider(
+        id_cols = c('sample', 'rep'), 
+        names_from = 'axis', 
+        values_from = 'score'
+      )
+  }
+    
+  gg <- df |> 
+    ggplot2::ggplot(mapping = ggplot2::aes(x = x, y = y)) +
     ggplot2::geom_hline(yintercept = 0, color = 'darkred') +
     ggplot2::geom_vline(xintercept = 0, color = 'darkred') +
-    ggplot2::geom_polygon(
-      ggplot2::aes(x, y, group = sample),
-      fill = NA,
-      color = 'black'
-    ) +
     ggplot2::labs(
-      x = paste0('PC', pc.x),
-      y = paste0('PC', pc.y),
+      x = paste0('PC', pc.x, ' (', round(prop.var[1] * 100, 1), '%)'),
+      y = paste0('PC', pc.y, ' (', round(prop.var[2] * 100, 1), '%)'),
       title = locus
     ) +
     ggplot2::theme_minimal()
+  
+  gg <- if(type == 'ellipse') {
+    gg + 
+      ggplot2::geom_polygon(
+        ggplot2::aes(group = sample), 
+        fill = NA, 
+        color = 'black'
+      ) 
+  } else {
+    gg +
+      ggplot2::geom_bin_2d(bins = num.bins) +
+      ggplot2::scale_fill_viridis_c(option = 'viridis') +
+      ggplot2::theme(legend.position = 'none')
+  }
+  print(gg)
   
   if(plot) print(gg)
   invisible(gg)
