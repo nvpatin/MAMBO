1737adbdf84a99b371349a4395c3ad1dee94a3ad
ericarcher <eric.ivan.archer@gmail.com>
2025-09-02 13:08:02 -0700

    cleanup of _devel folder

--- mambo/_devel/test legend.R
+++
@@ -1,7 +0,0 @@
-rm(list = ls())
-library(mambo)
-library(tidyverse)
-
-results <- readRDS('Lasker.20250819_124325.rds')
-
-plotPCs2(result, 'Lasker_18S', sample.df = sample.meta, ellipse.fill = 'depth_m')
